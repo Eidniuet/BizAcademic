7d4a8921e815ec57b4f5ea224f98865570e4c76d
Eidniuet <wenghongtong27@gmail.com>
2023-10-09 11:02:49 +0800

    finish

--- API.py
+++ API.py
@@ -3,36 +3,48 @@ import pandas as pd
 
 app = Flask(__name__)
 
-# Load data from the Excel file into dataframes
-item_df = pd.read_excel('data.xlsx', sheet_name='ItemInven')
-discount_df = pd.read_excel('data.xlsx', sheet_name='Discount')
+# Load the Excel sheets into dataframes
+item_df = pd.read_excel('SampleData.xlsx', sheet_name='ItemInven')
+discount_df = pd.read_excel('SampleData.xlsx', sheet_name='Discount')
 
-# Merge data based on DiscountCode
-merged_df = item_df.merge(discount_df, on='DiscountCode')
+# Function to calculate the discount price
+def calculate_discount(item_price, discount_percentage):
+    discount_price = item_price * (1 - discount_percentage)
+    return round(discount_price, 2)
 
-@app.route('/api/items', methods=['GET'])
-def get_items():
+# API endpoint to get all items with discount prices
+@app.route('/get_discounted_items', methods=['GET'])
+def get_discounted_items():
     items = []
 
-    for _, row in merged_df.iterrows():
+    for index, row in item_df.iterrows():
+        item_id = row['ItemID']
+        item_name = row['ItemName']
+        item_price = row['ItemPrice(RM)']
+        discount_code = row['DiscountCode']
+
+        # Find the discount percentage for the given code
+        discount_percentage = discount_df.loc[discount_df['DiscountCode'] == discount_code, 'Discount%'].values[0]
+
+        # Calculate the discount price
+        discount_price = calculate_discount(item_price, discount_percentage)
+
         item = {
-            'ItemID': int(row['ItemID']),
-            'ItemName': row['ItemName'],
-            'ItemPrice(RM)': float(row['ItemPrice(RM)']),
-            'DiscountCode': row['DiscountCode'],
-            'Discount%': row['Discount%']
+            'ItemID': item_id,
+            'ItemName': item_name,
+            'ItemPrice': item_price,
+            'DiscountCode': discount_code,
+            'DiscountPrice': discount_price
         }
-
-        # Calculate the DiscountPrice
-        item_price = item['ItemPrice(RM)']
-        discount_percentage = float(item['Discount%'][:-1]) / 100
-        discount_price = item_price * discount_percentage
-
-        item['DiscountPrice'] = discount_price
-
         items.append(item)
 
     return jsonify(items)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
