eb299b79ea7da2eed56043e83a2a9429e531864c
Eidniuet <wenghongtong27@gmail.com>
2023-10-10 18:22:33 +0800

    Update API.py

--- API.py
+++ API.py
@@ -3,30 +3,28 @@ import pandas as pd
 
 app = Flask(__name__)
 
-# Load the Excel sheets into dataframes
+
 item_df = pd.read_excel('SampleData.xlsx', sheet_name='ItemInven')
 discount_df = pd.read_excel('SampleData.xlsx', sheet_name='Discount')
 
-# Function to calculate the discount price
+
 def calculate_discount(item_price, discount_percentage):
     discount_price = item_price * (1 - discount_percentage)
     return round(discount_price, 2)
 
-# API endpoint to get all items with discount prices
-@app.route('/get_discounted_items', methods=['GET'])
+
+@app.route('/', methods=['GET'])
 def get_discounted_items():
     items = []
 
-    for index, row in item_df.iterrows():
+    for  index, row in item_df.iterrows():
         item_id = row['ItemID']
         item_name = row['ItemName']
         item_price = row['ItemPrice(RM)']
         discount_code = row['DiscountCode']
 
-        # Find the discount percentage for the given code
         discount_percentage = discount_df.loc[discount_df['DiscountCode'] == discount_code, 'Discount%'].values[0]
 
-        # Calculate the discount price
         discount_price = calculate_discount(item_price, discount_percentage)
 
         item = {
@@ -39,6 +37,7 @@ def get_discounted_items():
         items.append(item)
 
     return jsonify(items)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
